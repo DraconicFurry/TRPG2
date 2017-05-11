7848ee1192798f4756c75c891fe6c3f662143d3d
DraconicFurry <shibadruid@outlook.com>
2017-05-11 12:49:23 -0700

    Create Encounter.java

---
+++ Encounter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Encounter {
+  
+  Creature player;
+  ArrayList<Creature> enemies;
+  
+  public Encounter(Creature player, ArrayList<Creature> enemies) {
+    this.player = player;
+    this.enemies = enemies;
+  }
+  
+  public Encounter(Creature player, Creature[] enemies) {
+    this.player = player;
+    this.enemies = new ArrayList<Creature>();
+    for (int i = 0; i < enemies.length; i++) {
+      this.enemies.add(enemies[i];
+    }
+  }
+}
