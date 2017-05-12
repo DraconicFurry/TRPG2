df20c4c0a5c25822b3a6fbc27eec9eacb6af2bde
tydolt99 <tylerstrafford@hotmail.com>
2017-05-11 23:47:32 -0700

    Create StackItem.java

---
+++ Java Files/StackItem.java
@@ -0,0 +1,18 @@
+public class StackItem extends Item {
+
+    private int ammount;
+    // private int maxAmmount;
+    
+    public StackItem(String name, int value, int ammount) {
+        super(name, value);
+        this.ammount = ammount;
+    }
+    
+    public int getAmmount() {
+        return ammount;
+    }
+    
+    public String toString() {
+        return ("(Junk) " + super.getName() + ": " + super.getValue() + "g ");
+    }
+}   
