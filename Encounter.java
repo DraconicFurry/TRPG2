1d3efffd8ecb58a454ce5cc9aba6711a6c7b3bfb
DraconicFurry <shibadruid@outlook.com>
2017-05-11 22:49:20 -0700

    Update Encounter.java
    Added frame for run loop

--- Encounter.java
+++ Encounter.java
@@ -17,4 +17,21 @@ public class Encounter {
       		this.enemies.add(enemies[i];
     	}
 	}
+
+	public run() {
+		while(true) { //replace with end condition
+			playerAction();
+			for (int i = 0; i < enemies.size(); i++) {
+				enemyAction(enemies.get(i));
+			}
+		}
+	}
+	
+	public playerAction() {
+		//placeholder
+	}
+	
+	public enemyAction(Creature enemy) {
+		//placeholder
+	}
 }
