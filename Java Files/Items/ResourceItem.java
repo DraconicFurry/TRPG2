30b0c1c6e55f0301cf2351106553ba9712aa2032
tydolt99 <tylerstrafford@hotmail.com>
2017-05-12 01:36:21 -0700

    Update ResourceItem.java

--- Java Files/Items/ResourceItem.java
+++ Java Files/Items/ResourceItem.java
@@ -1,5 +1,9 @@
 public class ResourceItem extends StackItem {
 
+    public ResourceItem(String name, int value, int amount) {
+        super(name, value, amount);    
+    }
+    
     public String toString() {
         return ("(Resource Item) " + getName() + ": " + getValue() + "g");
     }
