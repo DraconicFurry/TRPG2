a5d6fe1b03c3d0b44e13c92831aaed527bfddf32
tydolt99 <tylerstrafford@hotmail.com>
2017-05-12 00:48:58 -0700

    Update NameGenerator.java

--- Java Files/Helpers/NameGenerator.java
+++ Java Files/Helpers/NameGenerator.java
@@ -1,17 +1,22 @@
 public class NameGenerator {
 
-    private String[] rangedPrefixes = new String["Enchanted", "Burning", "Frozen", "Electric", "Blooming", "Arcane", "Demonic", "Angelic", "Runic", "Apprentice's", "Master's"];
-    private String[] meleePrefixes = new String["Enchanted", "Burning", "Frozen", "Electric", "Blooming", "Arcane", "Demonic", "Angelic", "Runic", "Sharp", "Heavy", "Weighted", "Polished"];
+    private String[] rangedPrefixes = new String["Edgy", "Enchanted", "Burning", "Frozen", "Electric", "Blooming", "Arcane", "Demonic", "Angelic", "Runic", "Apprentice's", "Master's"];
+    private String[] meleePrefixes = new String["Edgy", "Enchanted", "Burning", "Frozen", "Electric", "Blooming", "Arcane", "Demonic", "Angelic", "Runic", "Sharp", "Heavy", "Weighted", "Polished"];
     private String[] rangedWeapons = new String["Staff", "Rod", "Wand", "Scepter", "Bow", "Gun", "Crossbow", "Javelin"];
-    private String[] meleePrefixes = new String["Sword", "Knife", "Mace", "Axe", "Spear", "Warhammer", "Tridemt"];
+    private String[] meleeWeapons = new String["Sword", "Knife", "Mace", "Axe", "Spear", "Warhammer", "Trident"];
     
-    public String generateWeapName(boolean isRanged) {
+    public String generateWepName(boolean isRanged) {
         String prefix;
         String type;
         
-        if (isRanged
+        if (isRanged) {
+            prefix = rangedPrefixes[(int)(Math.Random() * rangedPrefixes.length)];
+            type = rangedWeapons[(int)(Math.Random() * rangedWeapons.length)];
+        } else {
+            prefix = meleePrefixes[(int)(Math.Random() * meleePrefixes.length)];
+            type = meleeWeapons[(int)(Math.Random() * meleeWeapons.length)]
         }
         
-  
+        return (prefix + " " + type);
     }
 }
