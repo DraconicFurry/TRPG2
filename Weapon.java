81275821b9c1eb9abd839f979125de5732b15a13
tydolt99 <tylerstrafford@hotmail.com>
2017-05-11 23:02:40 -0700

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,20 +1,41 @@
 public class Weapon extends Item {
     
+    private String name;
     private int minDam;
     private int maxDam;
     private int damRange;
     private boolean isRanged;
     private boolean isMagic;
     
-    public Weapon(minDam, maxDam, isRanged, isMagic) {
+    public Weapon(String name, int minDam, int maxDam, boolean isRanged, boolean isMagic) {
+        this.name = name
         this.minDam = minDam;
         this.maxDam = maxDam;
+        this.damRange = maxDam - minDam;
         this.isRanged = isRanged;
         this.isagic = isMagic;
-        this.damRange = maxDam - minDam;
     }
     
     public int calculateAtk() {
         return (int)((Math.Random() * damRange) + minDam);
+    }
+    
+    public String toString() {
+        String range;
+        String magic;
+        
+        if (isRanged) {
+            range = "Ranged";        
+        } else {
+            range = "Melee";
+        }
+        
+        if (isMagic) {
+            magic = "Magical";
+        } else {
+            magic = "Physical";
+        }
+        
+        return (name + "(" + range + ", " + magic + "): " + minDam + "-" + maxDam);
     }
 }
