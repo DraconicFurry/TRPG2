dbd0587f78a8f2f48341c5d95fdbf7a085995688
DraconicFurry <shibadruid@outlook.com>
2017-05-11 22:43:19 -0700

    Update Encounter.java
    Changed tab size

--- Encounter.java
+++ Encounter.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 public class Encounter {
   
-  Creature player;
-  ArrayList<Creature> enemies;
+	Creature player;
+	ArrayList<Creature> enemies;
   
-  public Encounter(Creature player, ArrayList<Creature> enemies) {
-    this.player = player;
-    this.enemies = enemies;
-  }
+	public Encounter(Creature player, ArrayList<Creature> enemies) {
+		this.player = player;
+    	this.enemies = enemies;
+ 	}
   
-  public Encounter(Creature player, Creature[] enemies) {
-    this.player = player;
-    this.enemies = new ArrayList<Creature>();
-    for (int i = 0; i < enemies.length; i++) {
-      this.enemies.add(enemies[i];
-    }
-  }
+  	public Encounter(Creature player, Creature[] enemies) {
+    	this.player = player;
+    	this.enemies = new ArrayList<Creature>();
+    	for (int i = 0; i < enemies.length; i++) {
+      	this.enemies.add(enemies[i];
+    	}
+  	}
 }
