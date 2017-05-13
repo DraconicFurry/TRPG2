c96bddfe2b38bf5e0aaf151a8ce39af441fa7078
DraconicFurry <shibadruid@outlook.com>
2017-05-13 15:35:14 -0700

    Update SaveReader.java

--- Saves/SaveReader.java
+++ Saves/SaveReader.java
@@ -5,7 +5,8 @@ import Helpers.Input;
 
 public class SaveLoad {
 	
-	public Creature readSave(String fileName) {
+	public Creature readSave(String fileName) throws FileNotFoundException {
+		File toLoad = new File(fileName + ".txt");	
 		return new Creature("Test", 1, 50, 30);
 	}
 }
