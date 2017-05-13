a47a6e19f6e11c54d8e0a01c4c167ef4750fcd20
DraconicFurry <shibadruid@outlook.com>
2017-05-13 14:36:10 -0700

    Update Encounter.java

--- Java Files/Encounter.java
+++ Java Files/Encounter.java
@@ -45,8 +45,9 @@ public class Encounter {
 		while (!turnOver) {
 			System.out.println("1: Attack");
 			System.out.println("2: Skills");
-			System.out.println("3: Run");
-			int action = Input.validIntPrompt("action number", 3);
+			System.out.println("3: Items");
+			System.out.println("4: Run");
+			int action = Input.validIntPrompt("action number", 4);
 			switch (action) {
 				case 1:
 					for (int i = 0; i < enemies.size(); i++) {
@@ -64,10 +65,16 @@ public class Encounter {
 					System.out.println("Skills not yet implemented.");
 					break;
 				case 3:
+					System.out.println("Items not yet implemented.");
+					break;
+				case 4:
 					if ((int)(Math.random() * 2) == 1) { //50% chance
 						while(enemies.size() > 0) {
 							enemies.remove(0);
 						}
+						//Drop 50% of gold
+					} else {
+						System.out.println("Failed to run!");
 					}
 					turnOver = true;
 					break;
