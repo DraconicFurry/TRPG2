6e95e7da96ff9a5a3959a5b9a989ea47a8e2826a
DraconicFurry <shibadruid@outlook.com>
2017-05-13 16:06:36 -0700

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Player extends Creature {
+
+	private int gold;
+	private int XP;
+	//private Inventory inv;
+	
+	public Player(String name, int level, int HP, int MP) {
+		super(name, level, HP, MP);
+		gold = 0;
+		//inv = new Inventory();
+	}
+	
+	public Player(String name, int level, int XP, int HP, int MP, Weapon wep, Inventory inv, int gold) {
+		super(name, level, HP, MP, wep);
+		this.XP = XP;
+		this.gold = gold;
+		//this.inv = inv;
+	}
+	
+	private void levelUp() {
+		if (XP >= (level * 100)) {
+			XP -= level * 100;
+			level++;
+			//Increase stats
+			MHP += 50;
+			MMP += 25;
+			//Restore HP and MP
+			HP = MHP;
+			MP = MMP;
+		}
+	}
+}
