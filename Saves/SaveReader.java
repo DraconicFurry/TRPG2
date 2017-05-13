3dc0a8927dba6d80755a3a27b99b56647ae7c38e
DraconicFurry <shibadruid@outlook.com>
2017-05-13 15:30:24 -0700

    Create SaveReader.java

---
+++ Saves/SaveReader.java
@@ -0,0 +1,11 @@
+package Saves;
+import java.util.*;
+import java.io.*;
+import Helpers.Input;
+
+public class SaveLoad {
+	
+	public Creature readSave(File toLoad) {
+		return new Creature("Test", 1, 50, 30);
+	}
+}
