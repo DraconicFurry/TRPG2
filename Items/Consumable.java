a97d5bdb3255e250db6815747744458d7ac95b98
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 19:14:47 -0700

    Create Consumable.java

---
+++ Items/Consumable.java
@@ -0,0 +1,24 @@
+public abstract class Consumable extends StackItem {
+
+    private int consumeType;
+    private int effectScale
+    
+    public Consumable(int consumeType, int effectScale) {
+        this.consumeType = consumeType;
+        this.effectScale = effectScale;
+    }
+    
+    public int getType() {
+        return consumeType;
+    }
+    
+    public int getScale() {
+        return effectScale;
+    }
+   
+    public void use();
+    
+    public String toString() {
+        return ("(Consumable Type " + consumeType + ") " + getname() + "x" + getAmount() + ": " + getValue() + "g (" + (getValue() * amount) + "g total)");
+    }
+}
