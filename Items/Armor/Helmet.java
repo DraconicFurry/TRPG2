f6af39ebe666c1964179ce248c885af7e1a77567
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 23:14:40 -0700

    Create Helmet.java

---
+++ Items/Armor/Helmet.java
@@ -0,0 +1,13 @@
+imports Items.*;
+package Armor;
+
+public class Helmet extends Armor {
+
+    public Helmet(String name, int value, int defense) {
+      super(name, value, defense);
+    }
+    
+    public String toString() {
+        return ("(Helmet) " + getName() + ": " + getDefense() + " armor");
+    }
+}
