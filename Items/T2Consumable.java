491bb88390dfe391974ce6e39ce572588f2bcefc
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 19:29:22 -0700

    Create T2Consumable.java

---
+++ Items/T2Consumable.java
@@ -0,0 +1,9 @@
+public class T2Consumable extends Consumable {
+
+    public void use(Player player, List allies, List enemies) {
+        for (int i = 0; i < enemies.size(); i++) {
+            enemies.get(i).takeDamage(getScale());
+        }
+        remove(1);
+    }
+}
