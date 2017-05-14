f10d988aeb8fb9071f73f01154d547a3aacce2df
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 21:30:41 -0700

    Update AreaDamageConsumable.java

--- Items/AreaDamageConsumable.java
+++ Items/AreaDamageConsumable.java
@@ -1,4 +1,9 @@
 public class AreaDamageConsumable extends Consumable {
+    
+    public AreaDamageConsumable(int effectScale);
+        super(effectScale);
+        setType(2);
+    }
 
     public void use(Player player, List allies, List enemies) {
         for (int i = 0; i < enemies.size(); i++) {
