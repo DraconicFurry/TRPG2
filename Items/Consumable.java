9b867b75053d0af734bb14a6cd60f3b8d135760f
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 19:18:33 -0700

    Update Consumable.java

--- Items/Consumable.java
+++ Items/Consumable.java
@@ -15,6 +15,8 @@ public abstract class Consumable extends StackItem {
     public int getScale() {
         return effectScale;
     }
+    
+    public void use();
    
     public String toString() {
         return ("(Consumable Type " + consumeType + ") " + getname() + "x" + getAmount() + ": " + getValue() + "g (" + (getValue() * amount) + "g total)");
