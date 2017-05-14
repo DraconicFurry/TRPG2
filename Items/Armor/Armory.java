fd8976464b04a91e38a3bbbf24f03e5a92bc814a
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 23:41:55 -0700

    Create Armory.java

---
+++ Items/Armor/Armory.java
@@ -0,0 +1,46 @@
+import Items.*;
+package Armor;
+
+public class Armory; 
+
+    private Armor[] army;
+    
+    public Armory() {
+        army = new Armor[4]
+        army[0] = new Helmet("Starter Helmet", 1, 10);
+        army[1] = new Shoulderpads("Starter Shoulderpads", 1, 15);
+        army[2] = new Chestpiece("Starter Chestpiece", 1, 20);
+        army[3] = new Leggings("Starter Leggings", 1, 15);
+    }
+    
+    public Armor equip(Armor newArmor) {
+        if (newArmor instanceof Helmet) {
+            Armor temp = army[0];
+            army[0] = newArmor;
+            return temp;
+        } (newArmor instanceof Shoulderpads) {
+            Armor temp = army[1];
+            army[1] = newArmor;
+            return temp;
+        } (newArmor instanceof Chestpiece) {
+            Armor temp = army[2];
+            army[2] = newArmor;
+            return temp;
+        } (newArmor instanceof Leggings) {
+            Armor temp = army[3];
+            army[3] = newArmor;
+            return temp;
+        }
+        
+    public int totalDefense() {
+        int sum = 0
+        for (int i = 0; i < 4; i++) {
+            sum += army[i].getDefense();
+        }
+        return sum;
+    }
+}
+
+
+
+
