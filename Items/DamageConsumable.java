96f7e9a274e805e0dd54f88122d5b881241918e7
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 21:30:48 -0700

    Update DamageConsumable.java

--- Items/DamageConsumable.java
+++ Items/DamageConsumable.java
@@ -1,5 +1,10 @@
 public class DamageConsumable extends Consumable {
   
+    public DamageConsumable(int effectScale) {
+        super(effectScale);
+        setType(1);
+    }
+  
     public void use(Player player, List allies, List enemies) {
         int choice = Input.validIntPrompt("enemy target", enemies.size());
         enemies.get(choice).takeDamage(getScale());
