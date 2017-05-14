14f0d0117a59248b98053299f954d369175630dc
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 19:27:11 -0700

    Create T1Consumable.java

---
+++ Items/T1Consumable.java
@@ -0,0 +1,9 @@
+public class T1Consumable extends Consumable {
+  
+    public void use(Player player, List allies, List enemies) {
+        int choice = Input.validIntPrompt("enemy target", enemies.size());
+        enemies.get(choice).takeDamage(getScale());
+        remove(1);             
+        }
+    }
+}
