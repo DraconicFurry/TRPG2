3bf73b16259187aa4cb069b3029a71b80254f719
tydolt99 <tylerstrafford@hotmail.com>
2017-05-13 23:03:35 -0700

    Create Armor.java

---
+++ Items/Armor/Armor.java
@@ -0,0 +1,24 @@
+imports Items.*;
+
+public abstract class Armor extends Item {
+
+    int defense;
+    
+    public Armor(String name, int value, int defense) {
+        this.name = name;
+        this.value = value;
+        this.defense = defense;
+    }
+    
+    public String getName() {
+        return namel
+    }
+    
+    public int getValue*( {
+        return value;
+    } 
+    
+    public int getDefense() {
+        return defense;
+    }
+}
