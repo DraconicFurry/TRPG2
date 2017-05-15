448fceaa76a1263b6aebbd4960e09c7cb5007db1
DraconicFurry <shibadruid@outlook.com>
2017-05-15 12:54:02 -0700

    Update

--- RunGame.java
+++ RunGame.java
@@ -91,6 +91,8 @@ public class RunGame {
 				break;
 		}
 		Player player = new Player(name, 1, 0, 50, 30, wep, new Inventory(), 0);
+		Armory army = new Armory(player);
+		player.addArmory(army);
 		return player;
 	}
 	
