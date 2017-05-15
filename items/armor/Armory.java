bf36774f8130e8f2557cdd81780995e1261e4d94
tydolt99 <tylerstrafford@hotmail.com>
2017-05-15 12:44:44 -0700

    Update Armory.java

--- items/armor/Armory.java
+++ items/armor/Armory.java
@@ -3,13 +3,15 @@ package items.armor;
 public class Armory {
 
     private Armor[] army;
+    private Player owner;
     
-    public Armory() {
+    public Armory(Player owner) {
         army = new Armor[4];
         army[0] = new Helmet("Starter Helmet", 1, 10);
         army[1] = new Shoulderpads("Starter Shoulderpads", 1, 15);
         army[2] = new Chestpiece("Starter Chestpiece", 1, 20);
         army[3] = new Leggings("Starter Leggings", 1, 15);
+        this.owner = owner;
     }
     
     public Armor equip(Armor newArmor) {
