48a99e8dc21c1b16e94e0d587461e25a7a6b1067
DraconicFurry <ishiba@live.com>
2017-05-26 16:50:17 -0700

    Finished SaveLoad and status view in tavern

--- com/SaveLoad.java
+++ com/SaveLoad.java
@@ -1,6 +1,9 @@
 package com;
-import java.util.*;
+import java.util.*; 
 import java.io.*;
+import items.*;
+import items.armor.*;
+import items.consumables.*;
 @SuppressWarnings(value = { "unused", "resource" })
 
 public class SaveLoad {
@@ -12,7 +15,7 @@ public class SaveLoad {
 		return new Player("Test", 1, 50, 30);
 	}
 	
-	public static void saveGame(Creature player) throws FileNotFoundException, IOException {
+	public static void saveGame(Player player) throws FileNotFoundException, IOException {
 		File save = new File("SaveFiles/" + player.getName() + ".txt");
 		if (save.exists()) {
 			Scanner sc = new Scanner(save);
@@ -21,7 +24,30 @@ public class SaveLoad {
 		}
 		save.createNewFile();
 		PrintStream out = new PrintStream(save);
-		//Write data to new file
+		out.print(player.name + " " + player.level + " " + player.XP + " " + player.MHP + " " + player.HP + " " + player.MMP + " " + player.MP + " ");
+		Weapon wep = player.wep;
+		out.print(wep.getName() + " " + wep.getValue() + wep.getMin() + " " + wep.getMax() + " " + wep.getRanged() + " " + wep.getMagic() + " ");
+		Helmet hlm = (Helmet) player.army.army[0];
+		Shoulderpads sld = (Shoulderpads) player.army.army[1];
+		Chestpiece cst = (Chestpiece) player.army.army[2];
+		Leggings leg = (Leggings) player.army.army[3];
+		out.print(hlm.getName() + " " + hlm.getValue() + " " + hlm.getDefense() + " ");
+		out.print(sld.getName() + " " + sld.getValue() + " " + sld.getDefense() + " ");
+		out.print(cst.getName() + " " + cst.getValue() + " " + cst.getDefense() + " ");
+		out.print(leg.getName() + " " + leg.getValue() + " " + leg.getDefense() + " ");
+		Inventory inv = player.inv;
+		for (int i = 0; i < inv.getInv().length; i++) {
+			Item next = inv.getInv()[i];
+			if (next == null) {
+
+			} else if (next instanceof Consumable) {
+				out.print("cns " + next.getName() + " " + next.getValue() + " " + ((Consumable)next).getType()  + " " + ((Consumable)next).getScale() + " ");
+			} else if (next instanceof ResourceItem) {
+				out.print("res " + next.getName() + " " + next.getValue() + " " + ((ResourceItem)next).getAmount() + " ");
+			} else if (next instanceof StackItem) {
+				out.print("stk " + next.getName() + " " + next.getValue() + " " + ((ResourceItem)next).getAmount() + " ");
+			}
+		}
 	}
 	
 	public static void listFiles() throws FileNotFoundException {
